46223b3bbb3c8b53c7de1e493a0ec2f0339da6fa
Anas v <155738156+Anasmuhammedv@users.noreply.github.com>
2024-11-18 15:59:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ try {
 // Middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 
 // Session middleware
 app.use(session({
